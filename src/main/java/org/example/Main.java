03735cb2261c18fadb57fc99c61d8942d7683b64
summerr0-0 <summerr060@gamil.com>
2023-01-30 17:14:16 +0900

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
+
 }
